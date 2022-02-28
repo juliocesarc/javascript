c543ce41c51a3c09203d1224f286ca68bdebc7cd
juliocesarc <juliocostaesilva4@gmail.com>
2022-02-28 11:47:53 -0300

    ex045 3

--- ex045/script.js
+++ ex045/script.js
@@ -27,7 +27,17 @@ function ValidaCPF(cpfEnviado) {
 
 ValidaCPF.prototype.valida = function() {
     if (typeof this.cpfLimpo === 'undefined') return false;
+    if (this.cpfLimpo.length !== 11) return false;
+
+    const cpfParcial = this.cpfLimpo.slice(0, -2);
+    const digito1 = this.criaDigito();
+    
     return true;
+};
+
+ValidaCPF.prototype.criaDigito = function(cpfParcial) {
+    const cpfArray = Array.from(cpfParcial);
+    console.log(cpfArray);
 };
 
 const cpf = new ValidaCPF('705.484.450-52');
