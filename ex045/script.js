47bb7db347462a0054161a4964c8f44f92b95d84
juliocesarc <juliocostaesilva4@gmail.com>
2022-03-01 21:12:24 -0300

    ex045 finalizado

--- ex045/script.js
+++ ex045/script.js
@@ -28,18 +28,40 @@ function ValidaCPF(cpfEnviado) {
 ValidaCPF.prototype.valida = function() {
     if (typeof this.cpfLimpo === 'undefined') return false;
     if (this.cpfLimpo.length !== 11) return false;
+    if (this.isSequencia()) return false;
 
     const cpfParcial = this.cpfLimpo.slice(0, -2);
-    const digito1 = this.criaDigito();
-    
-    return true;
+    const digito1 = this.criaDigito(cpfParcial);
+    const digito2 = this.criaDigito(cpfParcial + digito1);
+
+    const novoCPF = cpfParcial + digito1 + digito2;
+
+    return this.cpfLimpo === novoCPF;
 };
 
 ValidaCPF.prototype.criaDigito = function(cpfParcial) {
     const cpfArray = Array.from(cpfParcial);
-    console.log(cpfArray);
+
+    let regressivo = cpfArray.length + 1;
+    const total = cpfArray.reduce((ac, val) => {
+        ac += (regressivo * Number(val));
+        regressivo--;
+        return ac;
+    }, 0);
+
+    const digito = 11 - (total % 11 );
+    return digito > 9 ? 0 : digito;
 };
 
-const cpf = new ValidaCPF('705.484.450-52');
-console.log(cpf.valida());
-//cpf.valida();
+ValidaCPF.prototype.isSequencia = function() {
+    const sequencia = this.cpfLimpo[0].repeat(11);
+    return sequencia === this.cpfLimpo;
+};
+
+const cpf = new ValidaCPF('705.484.450-52'); //705.484.450-52
+
+if (cpf.valida()) {
+    console.log('Cpf válido');
+} else {
+    console.log('Cpf inválido');
+}
